2471bab6559701ef4d3250fe74de2f42dbbb4629
alan.goncalves <alan.goncalves@celsolisboa.edu.br>
2025-05-19 20:18:07 -0300

    feat: cria Dashboard layout

---
+++ src/layouts/DashboardLayout.tsx
@@ -0,0 +1,17 @@
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+
+export default function DashboardLayout() {
+  return (
+    <div>
+      <Sidebar />
+      <div className="ml-64">
+        <Header />
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
