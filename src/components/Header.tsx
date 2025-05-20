ad64188121cf747857346a4583191301b8c412ec
alan.goncalves <alan.goncalves@celsolisboa.edu.br>
2025-05-20 08:16:55 -0300

    fix: remove componentes inutilizaveis

--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="bg-white shadow h-16 flex items-center px-4 ml-64">
-      <h1 className="text-xl font-semibold">Dashboard</h1>
-    </header>
-  );
-}
